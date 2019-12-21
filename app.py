a6baf37fb01e7638296608d6655ed78ee70ef439
胡绪沙 <huxusha@jczh56.com>
2019-12-21 15:47:09 +0800

    项目执行入口文件

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask
+import config
+from exts import db, swagger, api, mail
+
+from apps.user import user_bp
 
 
-app = Flask(__name__)
+def create_app():
+    # app初始化
+    app = Flask(__name__)
+    # 导入配置文件
+    app.config.from_object(config)
 
+    # 第三方库的实例注册到app中
+    db.init_app(app)
+    swagger.init_app(app)
+    api.init_app(app)
+    mail.init_app(app)
 
-@app.route("/")
-def index():
-    return "hello world"
+    # 蓝图注册
+    app.register_blueprint(user_bp, url_prefix="/user")
+
+    return app
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    start = create_app()
+    start.run()
