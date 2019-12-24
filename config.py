108e4a5effc8abd49f197b612c3fa114f0060eea
胡绪沙 <huxusha@jczh56.com>
2019-12-24 09:37:57 +0800

    项目信息配置

--- config.py
+++ config.py
@@ -6,10 +6,18 @@ DEBUG = True
 # 定义密钥
 SECRET_KEY = "HuXianSen_JYWuLiu"
 
-# 数据库设置
+# 数据库SQLAlchemy设置
 SQLALCHEMY_DATABASE_URI = "mysql://" + os.getenv("DATABASE_USER") + ":" + os.getenv("DATABASE_PASSWORD") \
                           + "@127.0.0.1:3306/wuliu?charset=utf8"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# 数据库MySQL配置
+MYSQL_HOST = "127.0.0.1"
+MYSQL_PORT = 3306
+MYSQL_USER = os.getenv("DATABASE_USER")
+MYSQL_PASSWORD = os.getenv("DATABASE_PASSWORD")
+MYSQL_DB = "wuliu"
+MYSQL_CHARTSET = "utf8"
 
 # 邮箱设置
 # 发送者邮箱的服务器地址
@@ -32,3 +40,9 @@ CACHE_REDIS_HOST = '127.0.0.1'
 CACHE_REDIS_PORT = 6379
 CACHE_REDIS_DB = ''
 CACHE_REDIS_PASSWORD = ''
+
+# Swagger UI配置
+SWAGGER = {
+    "title": "JY WuLiu API",
+    "uiversion": 3
+}
